f76d7e3a3bf95d47446569d39e3d83bfc466bb17
Pietro Giordano <giopi87@gmail.com>
2023-06-07 17:44:26 +0200

    aggiunta bonus

--- src/org/lessons/java/library/Main.java
+++ src/org/lessons/java/library/Main.java
@@ -1,9 +1,12 @@
 package org.lessons.java.library;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws BookCreateException {
+    public static void main(String[] args) throws BookCreateException, IOException {
         System.out.println("Salve. Quanti libri vuoi inserire in libreria?");
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
@@ -35,10 +38,48 @@ public class Main {
             library[i] = newBook;
         }
 
-        for (int i = 0; i < library.length; i++) {
-            System.out.println(library[i].toString());
+        for (Book book : library) {
+            System.out.println(book.toString());
         }
 
         scanner.close();
+
+        // BONUS
+        // creazione
+        try {
+            File newFile = new File("./file.txt");
+            if (newFile.createNewFile()) {
+                System.out.println("File creato");
+            } else {
+                System.out.println("File non creato");
+            }
+        } catch (IOException e) {
+            System.out.println("Errore in fase di creazione");
+        }
+
+        // scrittura
+        try {
+            FileWriter writer = new FileWriter("file.txt");
+            for (Book book : library) {
+                writer.write(book.toString());
+            }
+            writer.close();
+            System.out.println("Scrittura su file avvenuta correttamente");
+        } catch (Exception e) {
+            System.out.println("Errore scrittura su file");
+        }
+
+        // lettura su schermo
+        try {
+            File newFile = new File("./file.txt");
+            Scanner reader = new Scanner(newFile);
+            while (reader.hasNextLine()) {
+                System.out.println(reader.nextLine());
+            }
+            reader.close();
+            System.out.println("Lettura avvenuta correttamente");
+        } catch (Exception e) {
+            System.out.println("Errore lettura su file");
+        }
     }
 }
